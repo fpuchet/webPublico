67631e3afc51200f3301eb433e994cab76dde442
fpuchet <fpuchetag1900@alumno.ipn.mx>
2026-03-01 14:34:53 -0600

    CSS JS HTML COMPLETO

---
+++ recursos/js/main.js
@@ -0,0 +1,6 @@
+// Esperar a que el DOM esté cargado
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Portafolio de Alexander Pucheta cargado correctamente.");
+
+    // Puedes agregar efectos adicionales aquí, como scroll suave
+});
